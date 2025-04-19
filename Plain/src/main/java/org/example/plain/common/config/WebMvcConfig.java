959e2cd5befc38a7f40842059b8e7be44ea8d5ab
kimsee <ttungseui11@gmail.com>
2025-04-19 12:50:42 +0900

    refactor, WebMvcConfig.java 비활성화

--- Plain/src/main/java/org/example/plain/common/config/WebMvcConfig.java
+++ Plain/src/main/java/org/example/plain/common/config/WebMvcConfig.java
@@ -1,21 +1,21 @@
-package org.example.plain.common.config;
-
-import lombok.RequiredArgsConstructor;
-import org.example.plain.domain.meeting.interceptor.MeetingAuthInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-@RequiredArgsConstructor
-public class WebMvcConfig implements WebMvcConfigurer {
-
-    private final MeetingAuthInterceptor meetingAuthInterceptor;
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(meetingAuthInterceptor)
-                .addPathPatterns("/api/meetings/**")
-                .excludePathPatterns("/api/meetings/public/**");
-    }
-} 
+//package org.example.plain.common.config;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.example.plain.domain.meeting.interceptor.MeetingAuthInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//@RequiredArgsConstructor
+//public class WebMvcConfig implements WebMvcConfigurer {
+//
+//    private final MeetingAuthInterceptor meetingAuthInterceptor;
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(meetingAuthInterceptor)
+//                .addPathPatterns("/api/meetings/**")
+//                .excludePathPatterns("/api/meetings/public/**");
+//    }
+//}
